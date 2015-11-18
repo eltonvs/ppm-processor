0003a6e856601e4a78083762aa17d212d673bf38
Elton Viana <eltonvs@outlook.com>
2015-11-18 22:02:20 +0000

    Some documentation improvements

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 /**
- *  \file main.c
  *  \mainpage Processador de Imagens
  *  \author Elton Viana
  *  \date 2015
+ *  \version 1.0
+ *  \file main.c
+ *  \brief Arquivo principal
+ *
+ *  Arquivo que é compilado e executado
+ *
  */
 
-/** \brief Includes */
 #include <stdio.h> /** Biblioteca de funções de entrada/saída */
 #include <stdlib.h> /** Biblioteca de funções padrão */
 #include <string.h> /** Biblioteca de funções com strings */
 #include <ctype.h> /** Biblioteca de funções úteis */
 #include "libs/img_manip.h" /** Biblioteca para o processamento da imagem */
 
-/** \brief Função principal */
 int main() {
     /**
-     *  \details Função para receber o arquivo
+     *  \brief Função para receber o arquivo
      *  \param Nenhum
      *  \return 0
      */
